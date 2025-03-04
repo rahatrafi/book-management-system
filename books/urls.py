8a2bfb927870777249d7615b22390dba95612041
Rahat Rafi <rahatrafi@Rahats-MacBook-Pro.local>
2025-03-05 02:34:50 +0600

    first commit

---
+++ books/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import homepage, add_book, delete_book, edit_book
+
+urlpatterns = [
+    path('', homepage, name='homepage'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete/<int:book_id>/', delete_book, name='delete_book'),
+]
